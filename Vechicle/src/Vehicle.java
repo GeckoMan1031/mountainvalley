0ad51222fa3da45eafeeeffbda6b8844c20c5581
johnathan <johnathan@10.0.1.71>
2013-11-13 19:55:39 -0800

    This is my class demo.:

---
+++ Vechicle/src/Vehicle.java
@@ -0,0 +1,35 @@
+
+public class Vehicle
+{
+	String type;
+	int wheels;
+	boolean isFast;
+
+
+	public Vehicle(String t, int w, boolean f){
+
+		type = t;
+		wheels = w;
+		isFast = f;
+	}
+	public String getType(){
+
+		return type;
+
+	}
+	public int getWheels(){
+
+		return wheels;
+
+	}
+
+	public boolean getIsFast(){
+
+		return isFast;
+
+
+	}
+
+
+
+}
